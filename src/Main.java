b2d57dbbb599e90cd5312a4342ff44e5ca7092b2
Eryk <erykcorpo@gmail.com>
2021-03-26 14:28:59 +0100

    Added remove function

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,29 @@
 import java.util.Random;
+import java.util.Vector;
 
-public class Main
-{
-    public static void main(String[] args)
-    {
+public class Main {
+    public static void main(String[] args) {
+        QuadTree quadTree = new QuadTree(6, new Point(500, 500), new Dimension(1000, 1000));
 
+        for (int x = 0; x < 10000; x++) {
+            Vector<Point> points = new Vector<>();
+
+            for (int i = 0; i < 14000; i++) {
+                points.add(new Point((int) (Math.random() * 1000), (int) (Math.random() * 1000)));
+            }
+
+            for (int i = 0; i < 14000; i++) {
+                quadTree.insert(points.elementAt(i));
+            }
+
+            for (int i = 0; i < 14000; i++) {
+                quadTree.remove(points.elementAt(i));
+            }
+
+           // System.out.println(QuadTree.sumOfAllPoints);
+        }
     }
 }
+
+
+
--- src/QuadTree.java
+++ src/QuadTree.java
@@ -1,10 +1,11 @@
 import java.util.Vector;
 
-public class QuadTree
-{
+public class QuadTree {
     private final static int capacity = 10;
     private Vector<Point> points;
     private final QuadTree[] kids;
+    private int amountOfPoints = 0;
+    public static int sumOfAllPoints = 0;
 
     private final Point origin;
     private final Dimension dimension;
@@ -14,8 +15,7 @@ public class QuadTree
     private boolean isDivided;
     private final static int maxDepth = 5;
 
-    public QuadTree(int divideDepth, Point origin, Dimension dimension)
-    {
+    public QuadTree(int divideDepth, Point origin, Dimension dimension) {
         this.kids = new QuadTree[4];
         this.isDivided = false;
         this.divideDepth = divideDepth;
@@ -27,95 +27,90 @@ public class QuadTree
         this.boundaries = new Boundaries(new Point(origin.x - dimension.width / 2, origin.y - dimension.height / 2), new Dimension(dimension.width, dimension.height));
     }
 
-    public void insert(Point point)
-    {
-        if (!boundaries.contains(point))
-        {
+    public void insert(Point point) {
+        if (!boundaries.contains(point)) {
             return;
         }
 
-        if (isDivided)
-        {
+        if (isDivided) {
             insertToKid(point);
-        }
-        else
-        {
-            if (shouldBeDivided())
-            {
+        } else {
+            if (shouldBeDivided()) {
                 divide();
                 relocatePoints();
                 insertToKid(point);
-            }
-            else
-            {
-                insertToParent(point);
-            }
-        }
-    }
-
-    public boolean contains(Point point)
-    {
-        if (!boundaries.contains(point))
-        {
-            return false;
-        }
-
-        if (isDivided)
-        {
-            return kidsContain(point);
-        }
-        else
-        {
-            return parentContains(point);
-        }
-    }
-
-    private boolean parentContains(Point point)
-    {
-        for (Point existingPoint : points)
-        {
-            if (existingPoint.equals(point))
-            {
-                return true;
+            } else {
+                points.add(point);
+                amountOfPoints++;
+                sumOfAllPoints++;
             }
         }
-
-        return false;
     }
 
-    private boolean kidsContain(Point point)
-    {
-        for (QuadTree kid : kids)
-        {
-            if (kid.contains(point))
-            {
-                return true;
-            }
-        }
-
-        return false;
-    }
-
-    private void insertToParent(Point point)
-    {
-        points.add(point);
-    }
-
-    private void insertToKid(Point point)
-    {
-        for (QuadTree kid : kids)
-        {
+    private void insertToKid(Point point) {
+        for (QuadTree kid : kids) {
             kid.insert(point);
         }
     }
 
-    private boolean shouldBeDivided()
-    {
+    public boolean contains(Point point) {
+        if (!boundaries.contains(point)) {
+            return false;
+        }
+
+        if (isDivided) {
+            return kidsContain(point);
+        } else {
+            return parentContains(point);
+        }
+    }
+
+    private boolean parentContains(Point point) {
+        for (Point existingPoint : points) {
+            if (existingPoint.equals(point)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private boolean kidsContain(Point point) {
+        for (QuadTree kid : kids) {
+            if (kid.contains(point)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public void remove(Point point) {
+        if (!boundaries.contains(point)) {
+            return;
+        }
+
+        if (isDivided) {
+            for (QuadTree kid : kids) {
+                kid.remove(point);
+            }
+        } else {
+            if (points.contains(point)) {
+                points.remove(point);
+                amountOfPoints--;
+                sumOfAllPoints--;
+            }
+
+            if (amountOfPoints == 0) {
+                removeKids();
+            }
+        }
+
+    }
+
+    private boolean shouldBeDivided() {
         return points.size() >= capacity && divideDepth <= maxDepth;
     }
 
-    private void divide()
-    {
+    private void divide() {
         kids[0] = new QuadTree(divideDepth + 1, new Point(origin.x - dimension.width / 4, origin.y - dimension.height / 4), new Dimension(dimension.width / 2, dimension.height / 2));
         kids[1] = new QuadTree(divideDepth + 1, new Point(origin.x - dimension.width / 4, origin.y + dimension.height / 4), new Dimension(dimension.width / 2, dimension.height / 2));
         kids[2] = new QuadTree(divideDepth + 1, new Point(origin.x + dimension.width / 4, origin.y + dimension.height / 4), new Dimension(dimension.width / 2, dimension.height / 2));
@@ -124,18 +119,26 @@ public class QuadTree
         isDivided = true;
     }
 
-    private void relocatePoints()
-    {
-        for (Point point : points)
-        {
-            for (QuadTree kid : kids)
-            {
+    private void removeKids() {
+        kids[0] = null;
+        kids[1] = null;
+        kids[2] = null;
+        kids[3] = null;
+
+        isDivided = false;
+    }
+
+    private void relocatePoints() {
+        for (Point point : points) {
+            for (QuadTree kid : kids) {
                 kid.insert(point);
             }
         }
         points.clear();
-        points = null;
     }
 
-
+    public static int getSum()
+    {
+        return sumOfAllPoints;
+    }
 }
